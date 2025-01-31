6bb5324dd89d96832cd29c8f8737a9bdceadec4f
jinyan <jinyan@igetcool.com>
2025-01-31 15:16:22 +0800

    Update:入口文件添加store管理 src/Entry.tsx

--- src/Entry.tsx
+++ src/Entry.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
 import App from "./App";
+import "./index.scss";
+
+import { persistor, store } from "./Store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
