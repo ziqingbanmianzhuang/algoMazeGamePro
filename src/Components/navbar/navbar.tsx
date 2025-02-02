ba85f54ff4a9a7c40a2918bc863acf29f4be8018
jinyan <jinyan@igetcool.com>
2025-02-02 15:54:20 +0800

    Add:首页标题

---
+++ src/Components/navbar/navbar.tsx
@@ -0,0 +1,16 @@
+import Title from "../title/title";
+import classes from "./navbar.module.scss";
+
+export interface Props {
+  title: string;
+}
+
+function Navbar({ title }: Props) {
+  return (
+    <nav className={classes.navbar}>
+      <Title title={title} />
+    </nav>
+  );
+}
+
+export default Navbar;
